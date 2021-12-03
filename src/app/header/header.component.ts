83bafcecb9b7a89c07983185192417f0bf2a58dd
King-Martinien <martinienfokoue@yahoo.com>
2021-12-03 17:48:15 +0100

    implémentation du header avec switch theme operationnel

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,13 +3,28 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  switchTheme(event: Event): void {
+    let element = <HTMLButtonElement>event.currentTarget;
+
+    if (element.classList.contains('light')) {
+      let allLightElements = document.querySelectorAll('.light');
+      allLightElements.forEach((element) => {
+        element.classList.remove('light');
+        element.classList.add('dark');
+      });
+    } else {
+      let allDarkElements = document.querySelectorAll('.dark');
+      allDarkElements.forEach((element) => {
+        element.classList.remove('dark');
+        element.classList.add('light');
+      });
+    }
   }
-
 }
