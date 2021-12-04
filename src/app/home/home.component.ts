94b0b56b0b319802169570b5b2d132f437f9b060
King-Martinien <martinienfokoue@yahoo.com>
2021-12-04 11:27:50 +0100

    Ajout de : 'La barre de recherche et des la partie du filtre par region

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  regionFilter: String[] = ['Africa', 'America', 'Asia', 'Europe', 'Oceania'];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  activeFilterMenu(event: Event) {
+    let btn = <HTMLButtonElement>event.currentTarget;
+    let menu = btn.parentElement?.querySelector('.filter-menu');
+    menu?.classList.toggle('menu-active');
   }
-
 }
